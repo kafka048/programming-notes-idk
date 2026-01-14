7a63b0da13136f50e98979e9854c79495f9741bd
kafka048 <193783320+kafka048@users.noreply.github.com>
2026-01-14 21:09:36 +0530

    chore: remove node_modules and dist from repo

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `hello ${person}`;
-}
-const username = "amrit";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
